3bb068102f00624c01c38baf24616ad236d5ee92
jcastle <joseph.castle@gsa.gov>
2022-05-11 17:42:29 -0400

    completed chapt2 exercises from Python for Everybody

---
+++ chapt2_exercises.py
@@ -0,0 +1,39 @@
+# 1
+# x = 43
+# x = x + 1
+# print(x)
+
+# 2
+# inp = input('enter your name: ')
+# print('hello ' + inp)
+
+# 3
+# rate = input('Rate: ')
+# hours = input('Hours: ')
+# pay = round(float(rate) * float(hours))
+# print('Pay:', pay)
+
+# 4
+# width = 17
+# height = 12.0
+
+# print(width // 2)
+# print(type(width // 2))
+# print(width/2.0)
+# print(type(width/2.0))
+# print(height/3)
+# print(type(height/3))
+# print(1+2*5)
+# print(type(1+2*5))
+
+# 5
+# Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+
+# # Prompt user for Celsius temperature
+# celsius = input('Please provide a temperature in Celsius: ')
+
+# # Convert temperature to Fahrenheit
+# convert_to_Fahrenheit = round((float(celsius) * 9/5) + 32)
+
+# # Print out the converted temperature
+# print(convert_to_Fahrenheit)
